e534be0a73bf8b45aa981a3f664fae1b6263975d
Tom Bielawski <tfbielawski@protonmail.com>
2024-01-30 19:56:43 -0500

    Created components folder and moved components out of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header"
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  const description = reactDescriptions[genRandomInt(2)]
-  return (
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
-}
-
-// Destructure the props object
-function CoreConcept({title, description, image}){
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3> {title}</h3>
-      <p> {description}</p>
-    </li>
-  )
-}
 
 function App() {
+  function handleSelect(selectedButton) {
+    //components, jsx, props, state
+    console.log(selectedButton);    
+}
   return (
     <div>
       <Header />      
@@ -44,15 +20,27 @@ function App() {
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
-              />
+            />
               {/* shorter version of above. uses spread op, brings in the whole object */}
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
           </section>  
+          <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              {/* Anony arrow func passed as a value */}
+              {/* Ensures handleSelect not executed until clicked */}
+              {/* Allows for controlling exec, passing args, etc */}
+              <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            </menu>
+            Dynamic Content
+          </section>
         </main>
-      
     </div>
   );
 }
